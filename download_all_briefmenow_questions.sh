1624417bdcaf87b26fa9d496d14fe50c7c435309
Ubuntu <ubuntu@ip-172-31-42-143.us-west-2.compute.internal>
2018-10-20 14:18:11 +0000

    changes

--- download_all_briefmenow_questions.sh
+++ download_all_briefmenow_questions.sh
@@ -4,7 +4,6 @@ set -e
 node briefmenow_exams.js | jq -r '.[] | .href + "\t" + .text' | while IFS=$'\t' read -r -a line; do
    href=${line[0]} && tag="briefmenow "${line[1]}
    echo $tag
-#   node briefmenow_question_urls.js --exam-url $href  | jq -r '.' > "portions/$tag.json"
    node briefmenow_question_urls.js --exam-url $href  | jq -r --arg value "$tag" '[.[] | . + {"tag" : $value}]' > "portions/$tag.json"
    echo $tag" - done"
    cat "portions/$tag.json" | jq -r '.[] | .href + "\t" + .text' | while IFS=$'\t' read -r -a line; do
---
+++ exams_list.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node briefmenow_exams.js
